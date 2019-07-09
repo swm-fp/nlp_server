bcc6ae794b79ac3dd43bfb7464b8d91333112525
nuguziii <nuguziii@naver.com>
2019-07-09 21:37:37 +0900

    Create utils.py
    functions

---
+++ utils.py
@@ -0,0 +1,10 @@
+#===========================
+# keyword generator
+#===========================
+
+def get_keywordlist(title, url, highlight, memo, other_tags):
+    htmlParsing(url)
+    return keywords
+
+def htmlParsing(url): #return nouns from url
+    return nouns

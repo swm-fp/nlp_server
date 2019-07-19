0d4cd93827f4d4ee496ed38a0365fa2a8b2b7bd2
Chaen <nuguziii@naver.com>
2019-07-19 20:45:19 +0900

    change port

--- app.py
+++ app.py
@@ -54,6 +54,6 @@ def get():
     return "fail"
 
 if __name__=='__main__':
-	app.run(host='127.0.0.1',port=5002,debug=True)
+	app.run(host='0.0.0.0',port=5002,debug=True)
     #keyword_extractor("생성적 적대 신경망(GANs)에 대한 초보자용 가이드 (GANs)", "GANs을 이해하려면 생성(generative) 알고리즘이 작동하는 방식을 알아야 한다.")
 

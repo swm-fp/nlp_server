0dc0a8fc2286e85472846b4a78b1286e6ad86aac
Chae Eun Lee <nuguziii@naver.com>
2019-09-15 10:02:12 +0900

    구조 변경 및 형태소 분석 부분 추가

--- app.py
+++ app.py
@@ -7,49 +7,14 @@ from flask_cors import CORS, cross_origin
 from flask import request
 from flask import json
 from flask import Response
-import operator, re
-from konlpy.tag import Kkma
-from textblob import TextBlob
 import csv
+
+from utils import keyword_extractor
 
 app = Flask(__name__)
 cors = CORS(app)
 
 id = {"hello":"world"}
-
-def keyword_extractor(title, highlight):
-    konl = Kkma()
-    eng_title = TextBlob(re.sub("[^A-Za-z]", ",", title.strip())).noun_phrases
-    eng_highlight = TextBlob(re.sub("[^A-Za-z]", " ", highlight.strip())).noun_phrases
-    title_nouns = konl.nouns(title)
-    highlight_nouns = konl.nouns(highlight)
-
-    #line = re.sub("[^A-Za-z]", "", title.strip())
-
-    keyword_list = {i: 2 for i in title_nouns}
-    for i in highlight_nouns:
-        try:
-            keyword_list[i] +=1
-        except:
-            keyword_list[i] = 1
-
-    for i in eng_title:
-        keyword_list[i] = 2
-
-    for i in eng_highlight:
-        try:
-            keyword_list[i] +=1
-        except:
-            keyword_list[i] = 1
-
-    keyword_list = sorted(keyword_list.items(), key=operator.itemgetter(1), reverse=True)
-    
-    keywords={}
-    for i, k in enumerate(keyword_list):
-        if not k[0].isdigit():
-            keywords[str("k"+str(i))] = k[0]
-
-    return keyword_list, konl.pos(title)
 
 @app.route('/info/',methods=['POST', 'GET'])
 def get():
@@ -74,17 +39,16 @@ def get():
         with open('./data.csv', 'a', encoding='utf-8') as csvfile:
             fieldnames = ['title', 'url', 'keywords', 'result']
             wr = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            
-            #wr.writeheader()
-            wr.writerow(result)
-
+            wr.writeheader()
+            #wr.writerow(result)
 
         return Response(res, content_type='application/json; charset=utf-8')
+
     if request.method == 'GET':
         return json.dumps(id)
+        
     return "fail"
 
 if __name__=='__main__':
 	app.run(host='0.0.0.0',port=5002,debug=True)
-        # keyword_extractor("생성적 적대 신경망(GANs)에 대한 초보자용 가이드 (GANs)", "GANs을 이해하려면 생성(generative) 알고리즘이 작동하는 방식을 알아야 한다.")
 
